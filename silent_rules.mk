0e9b4e33496a6e4c7a8f0b44f0be18a655042b85
Etan Reisner <deryni@unreliablesource.net>
2015-04-30 23:44:09 -0400

    "Namespace" the new variables with '$(SR_PREFIX)_'.

--- silent_rules.mk
+++ silent_rules.mk
@@ -5,11 +5,11 @@ $(SR_PREFIX)_SILENT_VERBOSITY = -1
 
 V ?= $($(SR_PREFIX)_DEFAULT_VERBOSITY)
 
-SILENT := $(and $(filter $($(SR_PREFIX)_SILENT_VERBOSITY),$(V)),silent)
-DEFAULT := $(and $(filter $($(SR_PREFIX)_DEFAULT_VERBOSITY),$(V)),default)
-VERBOSE := $(and $(filter-out $($(SR_PREFIX)_SILENT_VERBOSITY) $($(SR_PREFIX)_DEFAULT_VERBOSITY),$(V)),verbose)
+$(SR_PREFIX)_SILENT := $(and $(filter $($(SR_PREFIX)_SILENT_VERBOSITY),$(V)),silent)
+$(SR_PREFIX)_DEFAULT := $(and $(filter $($(SR_PREFIX)_DEFAULT_VERBOSITY),$(V)),default)
+$(SR_PREFIX)_VERBOSE := $(and $(filter-out $($(SR_PREFIX)_SILENT_VERBOSITY) $($(SR_PREFIX)_DEFAULT_VERBOSITY),$(V)),verbose)
 
-VERBOSITY_MODE := $(or $(SILENT),$(DEFAULT),$(VERBOSE))
+$(SR_PREFIX)_VERBOSITY_MODE := $(or $($(SR_PREFIX)_SILENT),$($(SR_PREFIX)_DEFAULT),$($(SR_PREFIX)_VERBOSE))
 
 define vrule
 $(SR_PREFIX)_V_$(1) = $$($(SR_PREFIX)_V_$(1)_$$(V))
