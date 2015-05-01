d70b5798de30e03a54e321ecaf279a58594ac981
Etan Reisner <deryni@unreliablesource.net>
2015-05-01 00:03:37 -0400

    Improve safety when dealing with "odd" rule names.
    Switch to printf from echo.
    
    This switch to printf gets us safety when deailng with rules that start with a
    hyphen ('-').
    
    We also now escape single quotes in rule names so that we can print them
    correctly as well.

--- silent_rules.mk
+++ silent_rules.mk
@@ -18,14 +18,13 @@ $(SR_PREFIX)_NOT_VERBOSE := $(filter-out verbose,$($(SR_PREFIX)_VERBOSITY_MODE))
 define vrule
 $(SR_PREFIX)_V_$(1) = $$($(SR_PREFIX)_V_$(1)_$$(V))
 $(SR_PREFIX)_V_$(1)_ = $$($(SR_PREFIX)_V_$(1)_$$($(SR_PREFIX)_DEFAULT_VERBOSITY))
-$(SR_PREFIX)_V_$(1)_$($(SR_PREFIX)_DEFAULT_VERBOSITY) = @echo $(2);
+$(SR_PREFIX)_V_$(1)_$($(SR_PREFIX)_DEFAULT_VERBOSITY) = @printf -- %s$(and $(2),\\n) '$$(subst ','\'',$2)';
 $(SR_PREFIX)_V_$(1)_$($(SR_PREFIX)_SILENT_VERBOSITY) := @
 ifndef $(SR_PREFIX)_V_$(1)_$(V)
     $(SR_PREFIX)_V_$(1)_$(V) :=
 endif
 endef
 
-# -n prevents echo from printing an empty line.
-$(eval $(call vrule,AT,-n))
+$(eval $(call vrule,AT,))
 
 $(eval $(call vrule,GEN,GEN $$@))
